7a07d1c8f6cc55ea94a8140e50fda6f99b44a2e4
Henri Bergius <henri.bergius@iki.fi>
2013-05-22 23:41:02 +0200

    Component.io requires a main library file

---
+++ root/index.js
@@ -0,0 +1,6 @@
+/*
+ * This file can be used for general library features of {%= name %}.
+ *
+ * The library features can be made available as CommonJS modules that the
+ * components in this project utilize.
+ */
